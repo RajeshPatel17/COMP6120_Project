9f500f02cb68755406f5ee0714d173a8f1d49862
Rajesh Patel <rrp0019@auburn.edu>
2022-11-08 14:04:37 -0600

    got php working

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>COMP6120 Final Rajesh Patel rrp0019</title>
     <link rel="stylesheet" href="style.css">
     <script type="text/javascript">
         function submit(){
@@ -21,10 +21,13 @@
 
 <body class="home">
 
+    <h1>Database Query Form for Rajesh Patel (rrp0019) </h1>
     <label for="SQL Input">Input SQL Here:</label>
-    <input type="text" id="SQL Input" name="SQL Input">
-    <button type="button" onclick="submit();">Submit Query</button>
-    <button type="button" onclick="document.getElementById('SQL Input').value = '';">Clear Query</button>
+    <input type="text" id="SQL Input" name="SQL Input" class="inputBox">
+    <div class="buttons">
+        <button type="button" onclick="submit();">Submit Query</button>
+        <button type="button" onclick="document.getElementById('SQL Input').value = '';">Clear Query</button>    
+    </div>
     
 </body>
 </html>
