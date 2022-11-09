7258e630577d07d651798b68fedd67009c0ecec4
Rajesh Patel <rrp0019@auburn.edu>
2022-11-09 13:22:30 -0600

    select matches need to get data from db

--- api.php
+++ api.php
@@ -1,4 +1,5 @@
 <?php
+    //header("Content-Type: application/json");
     $input = $_GET['query'];
     if (empty($input)){
         echo "String is empty";
@@ -8,7 +9,7 @@
     
     $servername = "sysmysql8.auburn.edu";
     $username = "rrp0019";
-    $password = "rrp0019DBPass6120";
+    $password = "rrp0019dbpassword";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password);
@@ -17,6 +18,21 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
+
+    if (preg_match("/select/i", $input)){
+        $results = mysqli_query($conn, $input);
+        $row  = mysqli_fetch_array($results);
+        echo $results;
+        mysqli_close($conn);
+        //sql that contains select
+    } else {
+        //sql that does not contain select
+    }
+
+
+
+
+
     return $conn;
 
 ?>
